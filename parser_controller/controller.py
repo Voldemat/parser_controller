4f88a47a2fe2fdd890ed7d1a9359ebe2be7ac15e
Vladimir <vladimirdev635@gmail.com>
2022-03-24 03:50:30 +0300

    Initial commit

---
+++ parser_controller/controller.py
@@ -0,0 +1,19 @@
+class Controller:
+    """
+        Controller for parsing html to string.
+    """
+    def __init__(self, name:str):
+        """
+            Initialization of controller. 
+            :param name: The name of current controller.
+            :type str
+        """
+        self.name = name
+
+    def parse(html_string):
+        """
+            Parsing method.
+            :param html_string: The html string that will be parsed.
+            :type str
+        """
+        pass
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+
+name="parser_controller",
+    version="0.0.1",
+    author="Vladimir Vojtenko",
+    author_email="vladimirdev635@google.com",
+    description="Controller",
+    url="https://gitlab.com/p6679/parsercontroller",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    packages: ["parser_controller"]
+)
